3f168ade3fd2b25334c149ed4c1da2c2a3d7ce7f
jsguidotti <jefferson.guidotti@gmail.com>
2021-03-04 14:50:35 -0300

    Colocando Foto

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1>Guidotti´s Charcutarie<h1>
-Melhor casa de Charcutaria Artesanal da Zona Sul
+Melhor casa de Charcutaria Artesanal da Zona Sul<br>
+<img src=https://upload.wikimedia.org/wikipedia/commons/c/ce/Salame_Italiano_Toscano_Finocchiona.jpg
 
 Salames Italianos<br>
 Linguiças Blumenau<br>
