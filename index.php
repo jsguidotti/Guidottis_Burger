c111a609abf4c2f66805b8a08fd99911f65bf51d
jsguidotti <jefferson.guidotti@gmail.com>
2021-03-04 14:43:20 -0300

    Commit PHP

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1>Guidotti´s Charcutarie<h1>
 Melhor casa de Charcutaria Artesanal da Zona Sul
 
-Salames Italianos
-Linguiças Blumenau
-Salaminhos
-Defumados em geral
+Salames Italianos<br>
+Linguiças Blumenau<br>
+Salaminhos<br>
+Defumados em geral<br>
