1b7d774c51bf2adf1536da39b1d8c258624b4c63
jsguidotti <jefferson.guidotti@gmail.com>
2021-03-04 15:14:13 -0300

    Alterando Foto2

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<h1>Guidotti´s Burger<h1>
-Melhor casa de hamburger Artesanal da Zona Sul<br>
-<img src=https://respostas.sebrae.com.br/wp-content/uploads/2020/06/como_fazer_hamburguer_caseiro-806x440.jpg>
+<h1>Guidotti´s Charcuterie<h1>
+Melhor casa de Charcutaria Artesanal da Zona Sul<br>
+<img src=https://respostas.sebrae.com.br/wp-content/uploads/2020/06/como_fazer_hamburguer_caseiro-806x44
